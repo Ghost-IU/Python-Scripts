b89f2dc6b4f53fff4db90959605fc93cb1de040e
GHOST #IU <74893002+Ghost-IU@users.noreply.github.com>
2021-04-28 23:24:06 +0530

    Added 'hashlib' to convert to SHA1

--- Password Checker/main.py
+++ Password Checker/main.py
@@ -1,5 +1,7 @@
 import requests 
+import hashlib 
 
 url = 'https://api.pwnedpasswords.com/range/' + ' '
 res = requests.get(url)
 print(res)
+sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest).upper)
