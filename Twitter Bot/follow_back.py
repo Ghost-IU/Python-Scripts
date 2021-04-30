52b104f5305782632bcf27a0d05002a8bbdda1ec
GHOST #IU <74893002+Ghost-IU@users.noreply.github.com>
2021-04-30 22:44:59 +0530

    Update follow_back.py

--- Twitter Bot/follow_back.py
+++ Twitter Bot/follow_back.py
@@ -1,1 +1,23 @@
+import tweepy
+import time
 
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+user = api.me()
+printf(f"Hey {user.name()}")
+
+def limlit_handler(cursor):
+  try:
+    while True:
+      yield cursor.next()
+  except tweepy.RateLimitError:
+    time.sleep(500)
+ 
+user_name = 'NAME'   
+for follower in limit_handler(tweepy.Cursor(api.followers).items():
+  if follower.name == user_name:
+    follower.follow()
+    print(f'Started following {user_name}!')
+    break
