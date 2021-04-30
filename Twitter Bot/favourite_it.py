8a3b8372a163a78f79a0b7f63b5444e728592cc2
GHOST #IU <74893002+Ghost-IU@users.noreply.github.com>
2021-04-30 22:50:47 +0530

    Update favourite_it.py

--- Twitter Bot/favourite_it.py
+++ Twitter Bot/favourite_it.py
@@ -1,1 +1,28 @@
+import tweepy
+import time
 
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+user = api.me()
+printf(f"Hey {user.name()}")
+
+def limlit_handler(cursor):
+  try:
+    while True:
+      yield cursor.next()
+  except tweepy.RateLimitError:
+    time.sleep(500)
+ 
+search_string = 'YOUR_SEARCH_STRING'
+numberOfTweets = 2 #Can be chaged based on your needs.           
+
+for tweet in limlit_handler(tweepy.Cursor(api.search,search_string).items(numberOfTweets):
+  try:
+    tweet.favorite()
+    print("Tweet added to your favourites list!!") 
+  except tweepy.TweepError as err:
+    print(e.reason)
+  except StopIteration:
+    break
